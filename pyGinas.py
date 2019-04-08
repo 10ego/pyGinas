4b78d193b36218d141a77ae932c4f6b7b917dd3f
Ted Kim <ted.kim03@gmail.com>
2019-04-08 16:18:56 -0400

    Update pyGinas.py

--- pyGinas.py
+++ pyGinas.py
@@ -1,6 +1,5 @@
-# version 0.6
+# version 0.7
 import requests
-# import asyncio
 import multiprocessing
 from datetime import datetime
 import json
@@ -20,46 +19,31 @@ class pyGinasLogger():
 			raise IOError('%s: %s' % (path, exception.strerror))
 		return None
 	
-	def log(self, message):
+	def log(self, type, message):
 		timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S (%z)")
-		with open(self.logfile, type, 'a+') as log:
+		with open(self.logfile, 'a+') as log:
 			log.write('[{}] [{}] {}'.format(timestamp, type, message))
 			log.write('\n\n')
-	# def logUploads(self, message):
-		# timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S (%z)")
-		# with open(self.logfile_uploads, 'a+') as log:
-			# log.write('[{}] {}'.format(timestamp, message))
-			# log.write('\n\n')
 			
 class API():
-	def __init__(self):
+	def __init__(self, schema = None):
 		self.BASE_URL = "https://tripod.nih.gov/ginas/app/api/v1"
 		self.MY_URL = "https://srs.hres.ca/ginas/app/api/v1"
 		self.USERNAME = None
 		self.PASSWORD = None
+		self.ApprovalName = None
+		self.ApprovalPass = None
 		self.logger = pyGinasLogger('pyGinas')
-		self.SCHEMA = None
-	
-	def authenticate(self, username=None, password=None):
-		if username is None:
-			self.USERNAME = input("Username: ")
-		else:
-			self.USERNAME = username
-		if password is None:
-			self.PASSWORD = input("Password: ")
-		else:
-			self.PASSWORD = password
-			self.HEADER = {"auth-username" : self.USERNAME, "auth-password" : self.PASSWORD, "Content-Type":"application/json"}
-			self.logger.log("Msg", "User '{}' authenticated".format(self.USERNAME))
-	
-	def setSRS(self, uri):
+		self.SCHEMA = schema
+		
+	def _setMySRS(self, uri):
 		self.MY_URL = uri
 	
-	def setSchema(self, schema):
+	def _setSchema(self, schema):
 		self.SCHEMA = schema
 		self.logger.log("Msg", "Schema set to '{}'".format(self.SCHEMA))
 	
-	def _buildCode(self, code_id, code_system, ref=[], self_ref="", type = "PRIMARY",  deprecated = False, access=[]):
+	def _buildCode(self, code_id, code_system, url = None, ref=[], self_ref="", type = "PRIMARY",  deprecated = False, access=[]):
 		
 		code = {}
 		code['_self'] = self_ref  # Self link to reference the code
@@ -70,6 +54,7 @@ class API():
 		code['references'] = ref
 		code['access'] = access
 		code['uuid'] = str(uuid4()) # Must generate a new UUID for each new entry
+		code['url'] = url
 		
 		uuid_valid = False
 		while uuid_valid is False:
@@ -82,19 +67,55 @@ class API():
 				uuid_valid = True
 				self.logger.log("Msg", "Buliding custom code. Code '{}' assigned.".format(code['uuid']))
 		return code
-		
-	def validateSchema(self):
+
+	def _validateSchema(self):
 		if self.SCHEMA is None:
 			raise ValueError("Schema is set to None.")
 			self.logger.log("Error", "No schemas set.")
 		elif self.SCHEMA in ['references', 'edits', 'substances', 'structures', 'scheduledjobs', 'backups', 'xrefs', 'codes', 'values', 'vocabularies', 'keywords', 'names', 'payload', 'jobs']:
-			self.logger.log("Msg", "Schema set to {}."format(self.SCHEMA))
+			self.logger.log("Msg", "Schema set to {}.".format(self.SCHEMA))
 			pass
 		else:
 			raise ValueError('{} is not a supported schema'.format(self.SCHEMA))
 			self.logger.log("Error", "{} is not a supported schema".format(self.SCHEMA))
+	def _validateAuth(self):
+		if self.USERNAME is None or self.PASSWORD is None:
+			self.authenticate()
+	def _validateApprovalAuth(self):
+		if self.ApprovalName is None or self.ApprovalPass is None:
+			self._authenticateApproval()
+			
+	def _validateThis(self, data):
+		data = data.copy()
+		del data['approvalID']
+		yield data
+		
+	def _authenticateApproval(self, username=None, password=None): # Only for calling approval() function
+		if username is None:
+			self.ApprovalName = input("Approval Name: ")
+		else:
+			self.ApprovalName = username
+		if password is None:
+			self.ApprovalPass = input("Approval Password: ")
+		else:
+			self.ApprovalPass = password
+		self.ApprovalHEADER = {"auth-username" : self.ApprovalName, "auth-password" : self.ApprovalPass, "Content-Type":"application/json"}
+		self.logger.log("Msg", "Approval using account '{}' authenticated by '{}'".format(self.ApprovalName, self.USERNAME))
+		
+	def authenticate(self, username=None, password=None):
+		if username is None:
+			self.USERNAME = input("Username: ")
+		else:
+			self.USERNAME = username
+		if password is None:
+			self.PASSWORD = input("Password: ")
+		else:
+			self.PASSWORD = password
+		self.HEADER = {"auth-username" : self.USERNAME, "auth-password" : self.PASSWORD, "Content-Type":"application/json"}
+		self.logger.log("Msg", "User '{}' authenticated".format(self.USERNAME))
+		
 	def search(self, term):
-		self.validateSchema()
+		self._validateSchema()
 		if type(term) is str:
 			query_url = self.BASE_URL + "/{}/search?q={}".format(self.SCHEMA, term)
 			r = requests.get(query_url)
@@ -111,7 +132,7 @@ class API():
 							self.logger.log("Warning", "Exact match not found for '{}'. Returning next best match ({}).".format(term, name))
 					else:
 						print("Exact match found for '{}'.".format(term))
-						self.logger.log("Msg", "Exact match found for '{}'."format(term))
+						self.logger.log("Msg", "Exact match found for '{}'.".format(term))
 					return result[0]
 			else:
 				self.logger.log("Error", "Error code {}! Search call failed for '{}'.".format(r.status_code, term))
@@ -122,7 +143,7 @@ class API():
 		return None
 	
 	def getFull(self, uuid):
-		self.validateSchema()
+		self._validateSchema()
 		url = self.BASE_URL + "/{}({})?view=full".format(self.SCHEMA, uuid)
 		r = requests.get(url)
 		if 200 <= r.status_code < 300:
@@ -131,21 +152,14 @@ class API():
 		else:
 			self.logger.log("Error", "Error code {}! Search call failed for '{}' failed.".format(r.status_code, term))
 			print("Error", "Error code{}! Search call to '{}' failed.".format(r.status_code, url))
-			
 		return None
 	
-	def validateThis(self, data):
-		data = data.copy()
-		del data['approvalID']
-		yield data
-	
 	def validate(self, data):
-		self.validateSchema()
-		if self.USERNAME == None or self.PASSWORD == None:
-			self.authenticate()
+		self._validateSchema()
+		self._validateAuth()
 		url = self.MY_URL + "/{}/@validate".format(self.SCHEMA)
 		if 'approvalID' in data:
-			data = next(self.validateThis(data))
+			data = next(self._validateThis(data))
 		r = requests.post(url = url, headers=self.HEADER, data = json.dumps(data))
 		if 200 <= r.status_code < 300:
 			return r.json()
@@ -156,11 +170,10 @@ class API():
 			except Exception as e:
 				self.logger.log("Error", "Error code {}! Validation call for '{} 'failed. {} ERROR on args {}. See details - {}".format(r.status_code, data['_name'],type(e), e.args, r.json()))
 		return None
-		
+
 	def upload(self, data):
-		self.validateSchema()
-		if self.USERNAME == None or self.PASSWORD == None:
-			self.authenticate()
+		self._validateSchema()
+		self._validateAuth()
 		url = self.MY_URL + "/{}".format(self.SCHEMA)
 		r = requests.post(url = url, headers = self.HEADER, data = json.dumps(data))
 		if 200 <= r.status_code < 300:
@@ -172,33 +185,51 @@ class API():
 				print("Error code {}! Upload call for '{}' failed.".format(r.status_code, data['_name']))
 			except Exception as e:
 				self.logger.log("Error", "Error code {}! Upload call for '{}' failed. {} ERROR on args {}.".format(r.status_code, data['_name'], type(e), e.args))
-	
-	def update(self, data, code_data = None): # custom_code should be dictionary output of self._buildCode()
-		self.validateSchema()
-		if self.USERNAME == None or self.PASSWORD == None:
-			self.authenticate()
-		if code_data is not None:
-			data['codes'].append(code_data)
-		url = self.MY_URL + "/{}".format(self.SCHEMA)
-		r = requests.put(url = url, headers = self.HEADER, data = json.dumps(data))
-		if 200 <= r.status_code < 300:
-			self.logger.log("Msg", "Update success ({})!".format(data['_name']))
-			print("Update success ({})!".format(data['_name']))
-		else:
+				
+
+	# def update(self, data): # custom_code should be dictionary output of self._buildCode()
+		# self._validateSchema()
+		# if self.USERNAME is None or self.PASSWORD is None:
+			# self.authenticate()
+		
+		# url = self.MY_URL + "/{}".format(self.SCHEMA)
+		# r = requests.put(url = url, headers = self.HEADER, data = json.dumps(data))
+		# if 200 <= r.status_code < 300:
+			# self.logger.log("Msg", "Update success ({})!".format(data['_name']))
+			# print("Update success ({})!".format(data['_name']))
+		# else:
+			# try:
+				# self.logger.log("Error", "Error code {}! Update call for {} failed. See details - {}".format(r.status_code, data['_name'], r.json()))
+				# print("Error code {}! Update call to {} failed.".format(r.status_code, url))
+			# except Exception as e:
+				# self.logger.log("Error", "Update call to {} failed. {} ERROR on args {}".format(data['_name'], type(e), e.args))
+
+	def approve(self, uuid):
+		self._validateAuth()
+		self._validateApprovalAuth()
+		url = self.MY_URL + "/{}({})/@approve".format(self.SCHEMA, uuid)
+		if uuid is not None:
 			try:
-				self.logger.log("Error", "Error code {}! Update call for {} failed. See details - {}".format(r.status_code, data['_name'], r.json()))
-				print("Error code {}! Update call to {} failed.".format(r.status_code, url))
+				r = requests.get(url, headers = self.ApprovalHEADER)
+				if 200 <= r.status_code < 300:
+					print("Approval success ({})!".format(uuid))
+					self.logger.log("Msg", "Approval success ({})!".format(uuid))
+				else:
+					print("Approval failed ({})!".format(uuid))
+					self.logger.log("Error", "Approval failed ({})!".format(uuid))
 			except Exception as e:
-				self.logger.log("Error", "Update call to {} failed. {} ERROR on args {}".format(data['_name'], type(e), e.args))
-			
+				print("Approval failed ({})! {} ERROR on args {}.".format(uuid, type(e), e.args))
+				self.logger.log("Error", "Approval failed ({})! {} ERROR on args {}.".format(uuid, type(e), e.args))
+		else:
+			print("UUID has not been passed.")
+			self.logger.log("Error", "UUID has not been passed.")
+
 	def validateOnSearch(self, term):
-		if self.USERNAME == None or self.PASSWORD == None:
-			self.authenticate()
 		search_result = self.search(term)
-		if search_result != None:
+		if search_result is not None:
 			full_result = self.getFull(search_result['uuid'])
 			return self.validate(full_result)
-						
+
 	def uploadOnSearch(self, term):
 		if type(term) == tuple:
 			try:
@@ -210,32 +241,38 @@ class API():
 				self.logger.log("Error", "'{}' is a non-valid tuple. {} ERROR on args {}".format(term, type(e), e.args))
 		else:
 			custom_code = None
-		if self.USERNAME == None or self.PASSWORD == None:
-			self.authenticate()
 		search_result = self.search(term)
-		if search_result != None:
+		
+		if search_result is not None:
 			full_result = self.getFull(search_result['uuid'])
-			if custom_code != None:
-				code_data = self._buildCode(
-					code_id = custom_code,
-					code_system = code_sys
-					)
-				# *** Validate code uuid collision ***
+			val_result = self.validate(full_result) # Not sure if necessary. May be duplicated action if SRS is already validating on the backend.
+			if val_result is not None and val_result['valid'] is True:
+				if custom_code is not None:
+					code_data = self._buildCode(
+						code_id = custom_code,
+						code_system = code_sys,
+						)
+					full_result['codes'].append(code_data)
 				
-			else:
-				code_data = None
-			val_result = self.validate(full_result)
-			if val_result != None and val_result['valid'] is True:
-				full_result_copy = full_result.copy()
+				publicUUID = self._buildCode(
+						code_id = full_result['approvalID'],
+						code_system = "G-SRS UUID",
+						url = "https://tripod.nih.gov/ginas/app/substance/{}".format(full_result['uuid'].split("-")[0]),
+						# ref = [full_result['uuid']] # Must pass an array
+						)
+				full_result['codes'].append(publicUUID)
 				try:
 					del full_result['approvalID']
-				except:
-					pass
-				try:
 					self.upload(full_result)
-					self.update(full_result_copy, code_data)
-				except:
-					pass	
+					self.approve(full_result['uuid'])
+					
+				except Exception as e:
+					self.logger.log("Error", "Failed to build data for '{}'. {} ERROR on args {}".format(term, type(e), e.args))
+
+			else:
+				self.logger.log("Error", "Data validation error on uploading '{}'.".format(term))
+				print("Data validation error on uploading '{}'.".format(term))
+				
 	def bulkAction(self, action, bulkdata, multithread=False):
 	## bulkdata should be a list composed of just string values or tuples
 	## if it is tuples, the format should be (term, code, code_system)
@@ -254,7 +291,7 @@ class API():
 			valid_action = False
 		
 		if valid_action is True:
-			if self.USERNAME == None or self.PASSWORD == None:
+			if self.USERNAME is None or self.PASSWORD is None:
 				self.authenticate()
 			if multithread is True:
 				print("Executing {} jobs in multithread".format(len(bulkdata)))
@@ -275,14 +312,16 @@ class API():
 						# print("Completed {} of {} jobs".format(counter, len(jobs)))
 				# loop = asyncio.get_event_loop()
 				# loop.run_until_complete(main(bulkdata))
-				with multiprocessing.Pool(30) as p:
+				with multiprocessing.Pool(25) as p:
 					p.map(operation, bulkdata)
-				
+				print("Completed {} jobs".format(len(bulkdata)))
+				self.logger.log("Msg", "Completed {} jobs".format(len(bulkdata)))
+
 			else:
 				print("Executing {} jobs in singlethread".format(len(bulkdata)))
 				self.logger.log("Msg", "Executing {} jobs in singlethread".format(len(bulkdata)))
 				for data in bulkdata:
 					operation(data)
 					counter+=1
-			print("Completed {} of {} jobs".format(counter, len(bulkdata)))
-			self.logger.log("Msg", "Completed {} of {} jobs".format(counter, len(bulkdata)))
+				print("Completed {} of {} jobs".format(counter, len(bulkdata)))
+				self.logger.log("Msg", "Completed {} of {} jobs".format(counter, len(bulkdata)))
